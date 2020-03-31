deb6bd9ad33ab045a26da30b549625e228492d97
keegers <nikkikiga@gmail.com>
2020-03-31 00:20:50 -0700

    add javascript functionality, tests, and formatting

--- test/is-yes.test.js
+++ test/is-yes.test.js
@@ -1,16 +1,81 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import '../src/is-yes.js';
+import { isYes } from '../src/is-yes.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('Test whether y works', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const entry = 'y';
+    const expected = true;
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const result = isYes(entry);
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(result, expected);
+});
+
+test('Test whether n works', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const entry = 'n';
+    const expected = false;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(entry);
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, expected);
+});
+
+test('Test whether yassss works', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const entry = 'Yassss';
+    const expected = true;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(entry);
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, expected);
+});
+
+test('Test whether YEsSss works', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const entry = 'YEsSss';
+    const expected = true;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(entry);
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, expected);
+});
+
+test('Test whether typo works', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const entry = 'typo';
+    const expected = false;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = isYes(entry);
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, expected);
 });
